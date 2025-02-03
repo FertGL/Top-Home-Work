b66114e71b1ea3613c742e5490b45d8aea46dce7
gleb_fert <liant379@gmail.com>
2025-02-03 03:38:32 +0300

    remade sql

--- less_5_6SQL/sql_author/book/publish.sql
+++ less_5_6SQL/sql_author/book/publish.sql
@@ -1,32 +1,36 @@
+
+--АВТОР(MTM)
 CREATE TABLE author (
 	"id" BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-	"name" VARCHAR (50),
-	"book_id" BIGINT,
+	"name" VARCHAR (50)
 );
 
 
-
-INSERT INTO author("name", book_id)
-VALUES ('Егор Бугаенко', 1),
-	    ('Егор Бугаенко', 2),
-		('Роберт Мартин', 3);
+INSERT INTO author("name")
+VALUES ('Егор Бугаенко'),
+		('Роберт Мартин');
 
 
+
+--КНИГИ(MTM)
 CREATE TABLE book (
 	"id" BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	"name" VARCHAR (100),
 	"year" BIGINT,
-	"author_id" BIGINT,
+	"publish_id" BIGINT,
 
-	CONSTRAINT fk_book_author FOREIGN KEY (author_id) REFERENCES author(id)
+	CONSTRAINT fk_publish FOREIGN KEY (publish_id) REFERENCES publish(id)
+
 );
 
-INSERT INTO book("name","year",author_id)
+
+
+INSERT INTO book("name","year",publish_id)
 VALUES ('Наш код', 2019,1),
 		('Элегантные обьекты', 2017,1),
 		('Чистый код', 2008,2);
 
-
+--ИЗДАТЕЛЬСТВА
 CREATE TABLE publish (
 	"id" BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	"country" VARCHAR (100),
@@ -37,19 +41,19 @@ CREATE TABLE publish (
 
 INSERT INTO publish ("country","name")
 VALUES ('Россия', 'ПИТЕР'),
-		('Россия', 'ПИТЕР'),
-		('Россия', 'ПИТЕР');
+		('Не Россия', 'Не ПИТЕР');
+		
 
+--СВЯЗУЮЩАЯ ТАБЛИЦА АВТОРОВ И КНИГ(MTM)
+CREATE TABLE book_author (
+	book_id BIGINT,
+	author_id BIGINT,
 
-CREATE TABLE publish_book_id (
-	"book_id" BIGINT,
-	"publish_id" BIGINT,
-
-	CONSTRAINT fk_book_publish FOREIGN KEY (book_id) REFERENCES book(id),
-	CONSTRAINT fk_publish_book FOREIGN KEY (publish_id) REFERENCES publish(id)
+	CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES author(id),
+	CONSTRAINT fk_book FOREIGN KEY (book_id) REFERENCES book(id)
 );
 
-INSERT INTO publish_book_id (book_id, publish_id)
+INSERT INTO book_author (book_id, author_id)
 VALUES (1,1),
 		(2,1),
-		(3,1);
+		(3,2);
